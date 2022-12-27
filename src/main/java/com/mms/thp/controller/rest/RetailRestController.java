0df2cfead41ec46dcc126ddd280f9b7df8653903
Shubhasish <shubhasishb10@gmail.com>
2022-12-27 14:14:14 +0530

    Updated the license header in files.

--- src/main/java/com/mms/thp/controller/rest/RetailRestController.java
+++ src/main/java/com/mms/thp/controller/rest/RetailRestController.java
@@ -1,3 +1,9 @@
+/**
+ * Copyright (C) DreamTech Inc - All Rights Reserved Unauthorized copying of this file,
+ * via any medium is strictly prohibited Proprietary and confidential For any use
+ * of this software please write us to shubhasishb10@gmail.com.
+ */
+
 package com.mms.thp.controller.rest;
 
 import com.mms.thp.model.Retail;
